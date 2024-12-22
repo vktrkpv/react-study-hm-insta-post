8dc5211240a3e00c30600cc58581b34a75de6d12
“vktrkpv” <vktrkpb@icloud.com>
2024-12-22 15:59:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [picture, setPhoto] = useState(0);
+  const {id, photo, commentOne, commentTwo, commentThree, commentFour} = data[picture];
+
+
+  const previousPicture = () => {
+    setPhoto((picture => {
+      picture--;
+      if(picture < 0) {
+        return data.length-1;
+      }
+      return picture;
+    }))
+  }
+
+  const nextPicture = () => {
+    setPhoto(( picture => {
+      picture++;
+      if( picture > data.length -1 ){
+        picture = 0;
+      }
+      return picture;
+    }))
+  }
+
+  return(
+    <div>
+      <div className='container'>
+
+        <div className='btn-container'>
+          <button onClick={previousPicture} className='arr-btn'>&#8249;</button>
+        </div>
+
+        <div className='photo-container'>
+          <img src={photo} alt='bedroomPhoto' width="400px"/>
+        </div>
+
+        <div className='comments-container' key={id}>
+       
+          <div className='comment-box'>
+            <p>{commentOne}</p>
+            <button className='comm-btn'><i className="fa fa-trash-o"></i></button>
+          </div>
+
+          <div className='comment-box'>
+          <p>{commentTwo}</p>
+          <button className='comm-btn'><i className="fa fa-trash-o"></i></button>
+
+          </div>
+
+          <div className='comment-box'>
+          <p>{commentThree}</p>
+          <button className='comm-btn'><i className="fa fa-trash-o"></i></button>
+
+          </div>
+
+          <div className='comment-box'>
+          <p>{commentFour}</p>
+          <button className='comm-btn'><i className="fa fa-trash-o"></i></button>
+          </div>
+
+          <div>
+          <button onClick={() => setPhoto([])} className='arr-btn detele-btn'>
+            Delete All Comments
+          </button>
+        </div>
+
+        </div>
+
+        
+
+        <div className='btn-container'>
+          <button onClick={nextPicture} className='arr-btn' >&#8250;</button>
+
+        </div>
+
+      </div>
+      
+      
     </div>
-  );
+  )
+  
 }
 
 export default App;
